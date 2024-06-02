6e6d6b3fa6b65b6d89c97a6bee3ef977bd5fa35d
saurabh.dhebe <saurabh.dhebe@vernost.in>
2024-06-02 17:43:25 +0530

    fix

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs } from './schema.js';
 import db from './_db.js'
 
+const app = express();
+app.use(express.json()); 
 
 const resolvers = {
     Query: {
         games: () => {
             return db.games;
         },
+        game: (parent, args, context ) => {
+            return db.games.find((game)=> game.id === args.id);
+        },
         authors: () => {
             return db.authors;
+        },
+        author: (parent, args, context ) => {
+            return db.authors.find((author)=> author.id === args.id);
         },
         reviews: () => {
             return db.reviews;
@@ -19,17 +27,29 @@ const resolvers = {
         review: (parent, args, context ) => {
             return db.reviews.find((review)=> review.id === args.id);
         }
+    },
+    Game: {
+        reviews:(parent)=>{
+            return db.reviews.filter(r=>r.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews:(parent)=>{
+            return db.reviews.filter(r=>r.author_id === parent.id)
+        }
+    },
+    Review: {
+        author:(parent)=>{
+            return db.authors.find(a=>a.id === parent.author_id)
+        },
+        game:(parent)=>{
+            return db.games.find(g=>g.id === parent.game_id)
+        }
     }
 }
 
 // server setup
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
-
-const app = express();
-app.use(express.json()); 
+const server = new ApolloServer({typeDefs, resolvers})
 
 // Start ApolloServer
 const startApolloServer = async () => {
--- schema.js
+++ schema.js
@@ -1,24 +1,30 @@
 export const typeDefs = `#graphql
-    type Games { 
+    type Game { 
         id: ID!
         title: String!            
         platform: [String!]!
+        reviews:[Review!]
     }
     type Review { 
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author:Author!
     }
     type Author { 
         id: ID!
         name: String!
         verified: Boolean!
+        reviews:[Review!]!
     }
     type Query { 
-        games: [Games]
+        games: [Game]
+        game(id:ID!): Game
         reviews: [Review]
         review(id:ID!): Review
         authors: [Author]
+        author(id:ID!): Author
     }
 `
 
